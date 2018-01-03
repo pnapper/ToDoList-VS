e67b842d09963b9fe6db4779c205283a0bbe414f
patrick napper <patrick@patricknapper.com>
2018-01-03 08:22:34 -0800

    add edit and delete functionality for tasks.

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ToDoList.Models;
 
 namespace ToDoList
 {
