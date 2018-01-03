2962602a8fa865765989ba0e22dddd8466cb5800
patrick napper <patrick@patricknapper.com>
2018-01-02 16:38:09 -0800

    create initial ToDoList app in Visual Studio.

---
+++ ToDoList/Models/ToDoListContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace ToDoList.Models
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ToDoListContext
+    {
+        private readonly RequestDelegate _next;
+
+        public ToDoListContext(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ToDoListContextExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ToDoListContext>();
+        }
+    }
+}
